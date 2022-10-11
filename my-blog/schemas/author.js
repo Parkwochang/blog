6f16a27af8c290d759ad342dc3806163e2318451
DESKTOP-DNEL6IS\bizwiz <water9707@bizwizsystem.co.kr>
2022-10-11 17:39:18 +0900

    sanity init

---
+++ my-blog/schemas/author.js
@@ -0,0 +1,31 @@
+export default {
+  name: 'author',
+  type: 'document',
+  title: 'Author',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+    {
+      name: 'role',
+      title: 'Role',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+};
